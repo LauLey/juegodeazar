69a014a323521f72553873e9f7f58606d80cd02a
LauLey <226139236+LauLey@users.noreply.github.com>
2026-04-22 22:40:27 -0600

    feat: complete rock paper scissors game logic and scoring

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 function getComputerChoice () {
-    let ComputerChoice = Math.floor(Math.random()*3)
-    if (ComputerChoice===0)
+    let computerChoice = Math.floor(Math.random()*3)
+    if (computerChoice===0)
         return ("PIEDRA")
-    else if (ComputerChoice===1)
+    else if (computerChoice===1)
         return ("PAPEL")
-    else if (ComputerChoice===2)
-    return ("TIJERA")
+    else if(computerChoice===2)
+        return ("TIJERAS")
 }
 
 function getHumanChoice () {
-   let HumanChoice = prompt("¿PIEDRA, PAPEL O TIJERA?")
-   return HumanChoice.toUpperCase()
-
+   let humanChoice = prompt("¿PIEDRA, PAPEL O TIJERAS?")
+   return humanChoice.toUpperCase()
 }
 
 let humanScore = 0
 let computerScore = 0
 
 function playRound (humanChoice,computerChoice) {
-
+    if (humanChoice===computerChoice)
+        console.log("Lo mismo, empate")
+    else if ((humanChoice==="PIEDRA")&&(computerChoice==="TIJERAS"))
+        {console.log("Tijeras, tu ganas")
+        humanScore = humanScore + 1;}
+    else if ((humanChoice==="PAPEL")&&(computerChoice==="PIEDRA"))
+        {console.log("Piedra, me ganaste")
+            humanScore = humanScore + 1;}
+    else if((humanChoice==="TIJERAS")&&(computerChoice==="PAPEL"))
+        {console.log("Tu ganas, yo puse papel")
+        humanScore = humanScore + 1;}
+    else if ((humanChoice==="TIJERAS")&&(computerChoice==="PIEDRA"))
+        {console.log("Piedra.Gané!")
+        computerScore = computerScore + 1;}
+    else if((humanChoice==="PIEDRA")&&(computerChoice==="PAPEL"))
+        {console.log("Papel.Gané!")
+        computerScore = computerScore + 1;}
+    else if((humanChoice==="PAPEL")&&(computerChoice==="TIJERAS"))
+        {console.log("Tijeras.Gané!")
+        computerScore = computerScore + 1;}
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function gameRound () {
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice)
+}
 
-playRound(humanSelection, computerSelection);
 function playGame () {
+    gameRound ()
+    gameRound ()
+    gameRound ()
+    gameRound ()
+    gameRound ()
 
+    if (humanScore>computerScore)
+        console.log("Has ganado!")
+    else if (humanScore<computerScore)
+        console.log("Yo gané!")
+    else if (humanScore===computerScore)
+        console.log("Empatamos")
 }
 
-console.log(getComputerChoice())
+(playGame())
